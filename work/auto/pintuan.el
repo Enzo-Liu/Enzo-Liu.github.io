6b28e3acbae7851c1ccd1bc76ad8e7b89e3cf44a
Enzo Liu <liuenze6516@gmail.com>
2016-02-14 11:03:54 +0800

    Update published html files, committed by org-page.

---
+++ work/auto/pintuan.el
@@ -0,0 +1,48 @@
+(TeX-add-style-hook
+ "pintuan"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "presentation" "bigger")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontspec" "no-math") ("xeCJK" "BoldFont" "SlantFont" "AutoFakeBold=true" "AutoFakeSlant=true") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "fontspec"
+    "xeCJK"
+    "graphicx"
+    "xcolor"
+    "listings"
+    "geometry"
+    "verbatim"
+    "fixltx2e"
+    "longtable"
+    "float"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "marvosym"
+    "wasysym"
+    "amssymb"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:orgheadline2"
+    "sec:orgheadline5"
+    "sec:orgheadline6"
+    "sec:orgheadline13"
+    "sec:orgheadline15"))
+ :latex)
+
