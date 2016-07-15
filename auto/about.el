2558a4db89a15abe77df01572a942be0a0ef0754
Enzo Liu <liuenze6516@gmail.com>
2016-07-15 11:47:18 +0800

    Update published html files, committed by org-page.

---
+++ auto/about.el
@@ -0,0 +1,50 @@
+(TeX-add-style-hook
+ "about"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt" "a4paper")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "fontspec"
+    "xeCJK"
+    "graphicx"
+    "xcolor"
+    "listings"
+    "geometry"
+    "verbatim"
+    "fixltx2e"
+    "longtable"
+    "float"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "marvosym"
+    "wasysym"
+    "amssymb"
+    "hyperref"
+    "parskip"
+    "indentfirst")
+   (LaTeX-add-labels
+    "sec:orgheadline1"
+    "sec:orgheadline2"
+    "sec:orgheadline3"
+    "sec:orgheadline4"
+    "sec:orgheadline5"))
+ :latex)
+
---
+++ blog/auto/try-org-page.el
@@ -0,0 +1,54 @@
+(TeX-add-style-hook
+ "try-org-page"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "presentation")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontspec" "no-math") ("xeCJK" "BoldFont" "SlantFont" "AutoFakeBold=true" "AutoFakeSlant=true") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
+   (add-to-list 'LaTeX-verbatim-environments-local "common-lispcode")
+   (add-to-list 'LaTeX-verbatim-environments-local "common-lispcode*")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "fontspec"
+    "xeCJK"
+    "graphicx"
+    "xcolor"
+    "listings"
+    "geometry"
+    "verbatim"
+    "fixltx2e"
+    "longtable"
+    "float"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "marvosym"
+    "wasysym"
+    "amssymb"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:orgheadline1"
+    "sec:orgheadline3"
+    "sec:orgheadline2")
+   (LaTeX-add-xcolor-definecolors
+    "0.95"
+    "0,0,1"
+    "0.133,0.545,0.133"
+    "0.627,0.126,0.941"))
+ :latex)
+
